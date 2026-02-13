1576de367304ca56bc5e3e47c92938d3014b5f23
ed7543 <ed7543@student.uni-lj.si>
2026-02-13 21:10:13 +0100

    empty start

---
+++ listing/src/test/java/mk/rus/com/listing/ListingApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.rus.com.listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
